3d8ba14f0af80f09cc7add7c87c722eb0d3aedb1
sebash3151 <60160067+sebash3151@users.noreply.github.com>
2021-11-01 22:16:48 -0500

    Limitacion de objetos (#2)
    Se borraron todos los objetos, sprites, sonidos, scripts, etc que no se necesitaban en la escena a analizar. Dejando el proyecto lo mas clean posible para su refactoring.

---
+++ Harmony Master Despues/Assets/Scripts/Encontrados/ZonaSecreta.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZonaSecreta : MonoBehaviour
+{
+    public GameObject zona;
+    public GameObject zona2;
+    public musicalv1 musicalv1;
+
+    public AudioClip suspense;
+    AudioSource miedo;
+
+    private void Awake()
+    {
+        miedo = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            zona.SetActive(false);
+            zona2.SetActive(false);
+
+            if (musicalv1 != null)
+            {
+                musicalv1.nivel1.volume = 0f;
+            }
+            miedo.clip = suspense;
+            miedo.volume = 1f;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            zona.SetActive(true);
+            zona2.SetActive(true);
+            miedo.volume = 0f;
+
+            if (musicalv1 != null)
+            {
+                musicalv1.nivel1.volume = 1f;
+            }
+        }
+    }
+}
