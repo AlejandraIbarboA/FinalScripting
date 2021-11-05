1a325c7f1a41eb096ca97fd1f60cdcb70ddc3fd7
sebash3151 <60160067+sebash3151@users.noreply.github.com>
2021-11-04 21:27:56 -0500

    Organizacion de carpetas (#5)
    * update Nivel1
    
    organizacion de la jerarquia para tener todo mas a la mano y encarpetado
    
    * Eliminacion de animaciones y cambios de codigos
    
    Se agrego el state mashine para controlar el cambio de instrumentos
    Se eliminaron animaciones que no se usan
    Se aplicarion arreglos de datos para el spawn de objetos en los cofres y en los dialogos del gato
    
    * Update GameOverManager.cs
    
    update game audio system in game over
    
    * Organizacion de carpetas
    
    Se organizaron los scripts listos en carpetas

---
+++ Harmony Master Despues/Assets/Scripts/Arreglados/Chests/AbrirCasillero.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbrirCasillero : MonoBehaviour
+{
+    [SerializeField] GameObject[] objeto;
+    Animator anim;
+    private bool poderAbrir;
+    private bool abierto = false;
+    [SerializeField] Transform spawnPoint;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            poderAbrir = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            poderAbrir = false;
+        }
+    }
+
+    public void Sacar ()
+    {
+        if (!abierto)
+        {
+            int alea = Random.Range(0, objeto.Length);
+            Instantiate(objeto[alea], spawnPoint);
+        }
+
+    }
+
+
+    void Update()
+    {
+        if(poderAbrir)
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Sacar();
+                abierto = true;
+                anim.SetBool("abrido", abierto);              
+            }       
+        }
+    }
+}
