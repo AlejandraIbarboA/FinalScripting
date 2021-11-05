2ab4166dabdfb603ba0d944c9a0a532a09d04829
sebash3151 <sebash31510@gmail.com>
2021-11-04 21:25:44 -0500

    Organizacion de carpetas
    Se organizaron los scripts listos en carpetas

---
+++ Harmony Master Despues/Assets/Scripts/Arreglados/Enemies/Enemy_vida.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_vida : MonoBehaviour
+{
+    [SerializeField] float hp;
+    [SerializeField] GameObject boom;
+    [SerializeField] Transform explo;
+
+    [SerializeField] AudioSource audio;
+    [SerializeField] AudioClip deadSound;
+
+    public void hurt(float Damage)
+    {
+        this.hp -= Damage;
+        if (this.hp <= 0)
+        {
+            Instantiate(boom, explo.position, explo.rotation);
+            audio.clip = deadSound;
+            audio.Play();
+            Destroy(gameObject);
+        }
+    }
+}
