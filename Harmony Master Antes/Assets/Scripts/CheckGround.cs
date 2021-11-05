a0bd5880ddd3d907eafac29ecc48749318da8f64
sebash3151 <60160067+sebash3151@users.noreply.github.com>
2021-11-04 22:02:50 -0500

    version anterior del proyecto (#8)
    Juego Antes

---
+++ Harmony Master Antes/Assets/Scripts/CheckGround.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    private Player_Movement player;
+    public LayerMask WhatGround;
+    public Transform checkpoint;
+
+
+    void Start()
+    {
+        player = GetComponentInParent<Player_Movement>();
+        player.grounded = false;
+
+    }
+
+    private void Update()
+    {
+
+
+       if( Physics2D.OverlapCircle(this.checkpoint.position, 0.02f, this.WhatGround))
+        {
+            player.grounded = true;
+        }
+        else
+        {
+            player.grounded = false;
+        }
+    }
+ 
+}
---
+++ Harmony Master Antes/Assets/Scripts/barrera_quieta.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrera_quieta : MonoBehaviour
+{
+
+    public GameObject plataforma;
+    private Rigidbody2D plata;
+
+    private void Start()
+    {
+        plata = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            plata.constraints = RigidbodyConstraints2D.FreezeAll;
+
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            plata.constraints = RigidbodyConstraints2D.None;
+            
+        }
+
+    }
+}
